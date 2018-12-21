177cf3756c87e336023f8ca3094bab09ca506af7
Sentor60 <oguzozsancaktar@gmail.com>
2018-12-21 12:54:43 +0300

    -product client ve servis tamamlandı.

--- src/main/java/Client/ProductClient.java
+++ src/main/java/Client/ProductClient.java
@@ -1,9 +1,6 @@
 package Client;
 
-import com.proto.model.product.AddProductRequest;
-import com.proto.model.product.AddProductResponse;
-import com.proto.model.product.Product;
-import com.proto.model.product.ProductServiceGrpc;
+import com.proto.model.product.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -24,12 +21,23 @@ public class ProductClient {
                 .usePlaintext()
                 .build();
 
+        //addProduct(channel);
+        //readProduct(channel);
+        //updateProduct(channel);
+        //deleteProduct(channel);
+        listProducts(channel);
+
+    }
+
+
+    private void addProduct(ManagedChannel channel){
+
         ProductServiceGrpc.ProductServiceBlockingStub productClient = ProductServiceGrpc.newBlockingStub(channel);
 
         Product product = Product.newBuilder()
-                .setName("Cay")
-                .setPrice(1.0)
-                .setExplanation("Bildigin cay")
+                .setName("Oralet")
+                .setPrice(1.5)
+                .setExplanation("Kivili")
                 .build();
 
         AddProductResponse response = productClient.addProduct(AddProductRequest.newBuilder()
@@ -40,7 +48,54 @@ public class ProductClient {
         System.out.println("Received add Product response");
         System.out.println(response.toString());
 
+    }
 
+
+    private Product readProduct(ManagedChannel channel){
+
+        ProductServiceGrpc.ProductServiceBlockingStub productClient = ProductServiceGrpc.newBlockingStub(channel);
+
+        ReadProductResponse response = productClient.readProduct(ReadProductRequest.newBuilder()
+                .setProductId("5c1cb6034bd23422303531d6")
+                .build());
+
+       // System.out.println(response.getProduct().toString());
+
+        return response.getProduct();
+
+    }
+
+    private void updateProduct(ManagedChannel channel){
+
+        ProductServiceGrpc.ProductServiceBlockingStub productClient = ProductServiceGrpc.newBlockingStub(channel);
+
+        Product product = readProduct(channel);
+
+        product = product.toBuilder().setPrice(1.5).build();
+
+        UpdateProductResponse response = productClient.updateProduct(UpdateProductRequest.newBuilder().setProduct(product).build());
+
+        System.out.println(response.getProduct());
+    }
+
+    private void deleteProduct(ManagedChannel channel){
+
+        ProductServiceGrpc.ProductServiceBlockingStub productClient = ProductServiceGrpc.newBlockingStub(channel);
+
+        DeleteProductResponse response = productClient.deleteProduct(
+                DeleteProductRequest.newBuilder().setProductId("5c1cb6034bd23422303531d6").build());
+
+        System.out.println("Product deleted.");
+
+    }
+
+    private void listProducts(ManagedChannel channel){
+
+        ProductServiceGrpc.ProductServiceBlockingStub productClient = ProductServiceGrpc.newBlockingStub(channel);
+
+        productClient.listProducts(ListProductRequest.newBuilder().build()).forEachRemaining(
+                listProductsResponse -> System.out.println(listProductsResponse.getProduct().toString())
+        );
 
     }
 
--- src/main/java/Service/ProductServiceImpl.java
+++ src/main/java/Service/ProductServiceImpl.java
@@ -4,12 +4,14 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.proto.model.product.AddProductRequest;
-import com.proto.model.product.AddProductResponse;
-import com.proto.model.product.Product;
-import com.proto.model.product.ProductServiceGrpc;
+import com.mongodb.client.result.DeleteResult;
+import com.proto.model.product.*;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import static com.mongodb.client.model.Filters.eq;
 
 public class ProductServiceImpl extends ProductServiceGrpc.ProductServiceImplBase {
 
@@ -40,4 +42,152 @@ public class ProductServiceImpl extends ProductServiceGrpc.ProductServiceImplBas
         responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
+
+
+
+    @Override
+    public void deleteProduct(DeleteProductRequest request, StreamObserver<DeleteProductResponse> responseObserver) {
+        System.out.println("Received delete product request");
+
+        String productId  = request.getProductId();
+
+        DeleteResult result = null;
+
+        try{
+            result = collection.deleteOne(eq("_id", new ObjectId(productId)));
+        }catch (Exception e){
+            System.out.println("Product not found");
+            responseObserver.onError(
+                    Status.NOT_FOUND
+                            .withDescription("The product with the corresponding id was not found")
+                            .augmentDescription(e.getLocalizedMessage())
+                            .asRuntimeException()
+            );
+        }
+
+        if(result.getDeletedCount() == 0){
+            System.out.println("Product not found");
+            responseObserver.onError(
+                    Status.NOT_FOUND
+                            .withDescription("The product with the corresponding id was not found")
+                            .asRuntimeException()
+            );
+        }else{
+            System.out.println("Product deleted");
+            responseObserver.onNext(DeleteProductResponse.newBuilder()
+                    .setProductId(productId)
+                    .build());
+            responseObserver.onCompleted();
+        }
+    }
+
+    @Override
+    public void updateProduct(UpdateProductRequest request, StreamObserver<UpdateProductResponse> responseObserver) {
+
+        System.out.println("Received update product request");
+
+        Product product = request.getProduct();
+        String productId = product.getId();
+
+        Document result = null;
+
+        try{
+            result = collection.find(eq("_id", new ObjectId(productId)))
+                    .first();
+        }catch(Exception e){
+            responseObserver.onError(
+                    Status.NOT_FOUND
+                        .withDescription("The product with the correspondigh id was not found")
+                        .augmentDescription(e.getLocalizedMessage())
+                        .asRuntimeException()
+            );
+        }
+
+        if(result == null){
+            System.out.println("Blog not found");
+            responseObserver.onError(
+                    Status.NOT_FOUND
+                            .withDescription("The product with the correspondigh id was not found")
+                            .asRuntimeException()
+            );
+        }else{
+            Document replacement = new Document("name", product.getName())
+                    .append("price", product.getPrice())
+                    .append("explanation", product.getExplanation())
+                    .append("_id", new ObjectId(productId));
+
+            System.out.println("Replacing product in database...");
+
+            collection.replaceOne(eq("_id", result.getObjectId("_id")), replacement);
+
+            System.out.println("Replaced! Sending a response");
+
+            responseObserver.onNext(
+                    UpdateProductResponse.newBuilder()
+                            .setProduct(documentToProduct(replacement))
+                            .build()
+            );
+
+            responseObserver.onCompleted();
+        }
+
+    }
+
+    @Override
+    public void readProduct(ReadProductRequest request, StreamObserver<ReadProductResponse> responseObserver) {
+
+        System.out.println("Received read product request");
+        String productId = request.getProductId();
+
+        Document result = null;
+
+        try{
+            result = collection.find(eq("_id", new ObjectId(productId)))
+                    .first();
+        }catch (Exception e){
+            responseObserver.onError(
+                    Status.NOT_FOUND
+                        .withDescription("The product with the corresponding id was not found")
+                        .augmentDescription(e.getLocalizedMessage())
+                        .asRuntimeException()
+            );
+        }
+
+        if(result == null){
+            System.out.println("Product not found");
+            responseObserver.onError(
+                    Status.NOT_FOUND
+                            .withDescription("The product with the corresponding id was not found")
+                            .asRuntimeException()
+            );
+        } else{
+            System.out.println("Product found, sending response");
+            Product product = documentToProduct(result);
+            responseObserver.onNext(ReadProductResponse.newBuilder()
+                    .setProduct(product)
+                    .build());
+
+            responseObserver.onCompleted();
+        }
+    }
+
+    @Override
+    public void listProducts(ListProductRequest request, StreamObserver<ListProductsResponse> responseObserver) {
+        System.out.println("Received List Blog Request");
+
+        collection.find().iterator().forEachRemaining(document -> responseObserver.onNext(
+                ListProductsResponse.newBuilder().setProduct(documentToProduct(document)).build()
+        ));
+
+        responseObserver.onCompleted();    }
+
+    private Product documentToProduct(Document document){
+        return Product.newBuilder()
+                .setName(document.getString("name"))
+                .setPrice(document.getDouble("price"))
+                .setExplanation(document.getString("explanation"))
+                .setId(document.getObjectId("_id").toString())
+                .build();
+    }
+
 }
